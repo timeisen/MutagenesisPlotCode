2108e15560e28ed8bcf719a0a1a6c0b5502019de
timeisen <timeisen@Tim-Eisen-MBPTB16-2019.local>
2022-11-04 16:49:45 -0700

    fixed a bug in the REfinder.py script

--- NNS_mutation_code/REfinder.py
+++ NNS_mutation_code/REfinder.py
@@ -20,9 +20,11 @@ def main():
 	enzymeF = eval(args.enzyme)
 
 	with open(args.input, 'r') as f1: #main code block that parses the fastq file (.extendedFrags )
+		print('Searching for:', enzymeF, 'site:', enzymeF.site)
 		for record in SeqIO.parse(f1, "fasta"):
 			if len(enzymeF.search(record.seq)) > 0: 
-				pos_list = [m.start() for m in re.finditer(enzymeF.site, str(record.seq))]
+				pos_list = [m.start() for m in re.finditer(enzymeF.site, str(record.seq).upper())]
+				pos_list = pos_list + [m.start() for m in re.finditer(str(Seq(enzymeF.site).reverse_complement()), str(record.seq).upper())]
 				print(record.id, 'Positions:', pos_list)
 
 if __name__ == "__main__":
